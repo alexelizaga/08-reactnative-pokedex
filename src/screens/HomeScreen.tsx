fa557ee42539ca1e057246f7126d211d689d89f2
alexelizaga <alexelizaga@icloud.com>
2022-04-17 17:44:25 +0200

    249

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -4,6 +4,7 @@ import { useSafeAreaInsets } from 'react-native-safe-area-context';
 import { usePokemonPaginated } from '../hooks/usePokemonPaginated';
 
 import { styles } from '../theme/appTheme';
+import { FadeInImage } from '../components/FadeInImage';
 
 export const HomeScreen = () => {
   const { top } = useSafeAreaInsets();
@@ -22,8 +23,8 @@ export const HomeScreen = () => {
         keyExtractor={ (pokemon) => pokemon.id }
         showsVerticalScrollIndicator={ false }
         renderItem={ ({item}) => (
-          <Image
-            source={{ uri: item.picture}}
+          <FadeInImage
+            uri={item.picture}
             style={{
               width: 100,
               height: 100
