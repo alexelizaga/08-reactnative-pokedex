91cf2fa836ba5923a238dd3fcacef25032276a6c
alexelizaga <alexelizaga@icloud.com>
2022-04-17 17:01:02 +0200

    246

--- src/hooks/usePokemonPaginated.tsx
+++ src/hooks/usePokemonPaginated.tsx
@@ -1,15 +1,21 @@
-import { useEffect, useRef } from 'react';
+import { useEffect, useRef, useState } from 'react';
 
 import { pokemonApi } from '../api/pokemonApi';
+import { PokemonPaginatedResponse, Result, SimplePokemon } from '../interfaces/pokemonInterfaces';
 
 export const usePokemonPaginated = () => {
+  const [simplePokemonList, setSimplePokemonList] = useState<SimplePokemon[]>([]);
   const nextPageUrl = useRef('https://pokeapi.co/api/v2/pokemon?limit=40')
 
   const loadPokemons = async() => {
+    const resp = await pokemonApi.get<PokemonPaginatedResponse>(nextPageUrl.current);
+    nextPageUrl.current = resp.data.next;
+    mapPokemonList( resp.data.results );
+  }
 
-    const resp = await pokemonApi.get(nextPageUrl.current);
-    console.log(resp.data);
+  const mapPokemonList = (pokemonList: Result[]) => {
 
+    pokemonList.forEach( p => console.log(p.name) );
   }
 
   useEffect(() => {
@@ -17,5 +23,7 @@ export const usePokemonPaginated = () => {
   }, [])
   
 
-  return {}
+  return {
+    simplePokemonList
+  }
 }
