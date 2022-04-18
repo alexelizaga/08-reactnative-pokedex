4ca7546fd041a74bd5e1a31be9bb088aa1e1e3f4
alexelizaga <alexelizaga@icloud.com>
2022-04-18 15:41:03 +0200

    259

--- src/components/PokemonDetails.tsx
+++ src/components/PokemonDetails.tsx
@@ -91,6 +91,50 @@ export const PokemonDetails = ( { pokemon }: Props ) => {
         </View>
       </View>
 
+      <View style={ styles.container }>
+        <Text style={ styles.title }>Stats</Text>
+        <View>
+          {
+            pokemon.stats.map( ( stat, i ) => (
+              <View
+                key={ stat.stat.name + i }
+                style={{ flexDirection: 'row' }}
+              >
+                <Text
+                  style={[
+                    styles.regularText,
+                    {
+                      marginRight: 10,
+                      width: 150
+                    } 
+                  ]}
+                >
+                  { stat.stat.name }
+                </Text>
+                <Text
+                  style={[
+                    styles.regularText,
+                    { fontWeight: 'bold' }
+                  ]}
+                >
+                  { stat.base_stat }
+                </Text>
+              </View>
+            ))
+          }
+        </View>
+
+        <View style={{
+          marginBottom: 20,
+          alignItems: 'center'
+        }}>
+          <FadeInImage
+            uri={ pokemon.sprites.front_default }
+            style={ styles.basicSprite }
+          />
+        </View>
+      </View>
+
     </ScrollView>
   )
 }
